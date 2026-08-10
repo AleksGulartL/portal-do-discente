b681f6bffe4fa463ba3626b79aa74afbe2947953
AleksGulartL <aleksander.lopes@estudante.uffs.edu.br>
2026-08-10 09:50:38 -0300

    att4

--- projeto/src/test/java/LoginTest.java
+++ projeto/src/test/java/LoginTest.java
@@ -21,7 +21,7 @@ public class LoginTest {
         a.navegarMenuDiscente("Ensino", "trancamento de matrícula");
         a.trancamentoDeMatriculaCCR("03854413041", "", "03112005");
         // a.matriculaExtraordinaria("Chapecó", "03854413041", "123", "03112005");
-        // a.suspensaoDoPrograma("03854413041", "Aleks15872704", "03112005");
+        // a.suspensaoDoPrograma("03854413041", "", "03112005");
     }
 
     @After
